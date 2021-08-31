ea4974200e52d03cd94715fc1795b56db7152fc2
!AC4G ʕ•́ᴥ•̀ʔっ <76955327+AC4GPutin@users.noreply.github.com>
2021-08-31 20:57:02 +0200

    Update database.php

--- config/database.php
+++ config/database.php
@@ -1,7 +1,7 @@
 <?php
 
 define('DB_DATABASE','bansai');
-define('DB_USERNAME','ac4g');
-define('DB_PASSWORD','hToGTqj5@)w_W8Kb');
+define('DB_USERNAME','**********');
+define('DB_PASSWORD','****************');
 define('DB_HOST','localhost');
 define('DB_CHARSET','utf8');
